80ac28d713f9fbabd6eeee5d283599f2ca29ef31
halabboo <husain.akarim@gmail.com>
2026-05-14 20:35:51 +0300

    update the imports for redis test

--- crates/nova-core/tests/redis_store_integration.rs
+++ crates/nova-core/tests/redis_store_integration.rs
@@ -1,6 +1,6 @@
 #![cfg(feature = "redis-store")]
 
-use nova_resilience_store::{RedisStore, ResilienceStore};
+use nova_core::{RedisStore, ResilienceStore};
 use std::env;
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
