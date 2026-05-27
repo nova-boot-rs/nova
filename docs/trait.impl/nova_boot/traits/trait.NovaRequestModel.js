d318f02bd6800f8ec98f4159ffbb989cb5fa04e5
halabboo <husain.akarim@gmail.com>
2026-05-28 00:20:10 +0300

    change core to boot

--- docs/trait.impl/nova_boot/traits/trait.NovaRequestModel.js
+++ docs/trait.impl/nova_boot/traits/trait.NovaRequestModel.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["demo",[["impl NovaRequestModel for <a class=\"struct\" href=\"demo/controller/struct.CreateUser.html\" title=\"struct demo::controller::CreateUser\">CreateUser</a>"],["impl NovaRequestModel for <a class=\"struct\" href=\"demo/controller/struct.MessageReceived.html\" title=\"struct demo::controller::MessageReceived\">MessageReceived</a>"]]]]);
+    var implementors = Object.fromEntries([["demo",[["impl <a class=\"trait\" href=\"nova_boot/traits/trait.NovaRequestModel.html\" title=\"trait nova_boot::traits::NovaRequestModel\">NovaRequestModel</a> for <a class=\"struct\" href=\"demo/controller/struct.CreateUser.html\" title=\"struct demo::controller::CreateUser\">CreateUser</a>"],["impl <a class=\"trait\" href=\"nova_boot/traits/trait.NovaRequestModel.html\" title=\"trait nova_boot::traits::NovaRequestModel\">NovaRequestModel</a> for <a class=\"struct\" href=\"demo/controller/struct.MessageReceived.html\" title=\"struct demo::controller::MessageReceived\">MessageReceived</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[343]}
+//{"start":57,"fragment_lengths":[601]}
